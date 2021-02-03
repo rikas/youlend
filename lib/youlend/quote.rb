aa45dfc654017403621b3e2670696f3394df98e3
rikas <oterosantos@gmail.com>
2021-02-03 18:11:33 +0000

    Return the errors response correctly

--- lib/youlend/quote.rb
+++ lib/youlend/quote.rb
@@ -6,6 +6,9 @@ module Youlend
     def self.pre_qualification(params)
       response = Youlend.connection.post('/prequalification/Requests', :prequalification, params)
 
+      # If there are errors in the data just return the response right away
+      return response if response.status == 422
+
       # If we got back a response but all the funded amounts are 0.0 it means that the load was
       # actually rejected! We'll replace the response body with an error message instead and change
       # the success code.
